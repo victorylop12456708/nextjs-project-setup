7145de9f3bd8bbbabddb302eecd8da3ca5794bf3
victorylop12456708 <victorylopoficial@gmail.com>
2025-07-26 22:18:26 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753579105718

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_315f5b10._.js",
+  "static/chunks/node_modules_next_dist_compiled_react_26ec58f1._.js"
+],
+    source: "dynamic"
+});
